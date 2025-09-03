1b6586d4c81dddb1c7e3cb3f50780ba30b78d38d
Oghene-Suvwe <oghenesuvweomashone@hotmail.com>
2025-09-03 10:27:56 +0100

    fix(build): Correct import path in gatsby-browser.js

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,6 +1,6 @@
 import React from 'react';
 
-import { NotificationProvider } from './src/context/AddItemNotificationProvider';
+import { NotificationProvider } from './src/context/AddItemNotificationProvider.js';
 
 export const wrapRootElement = ({ element }) => (
   <NotificationProvider>{element}</NotificationProvider>
