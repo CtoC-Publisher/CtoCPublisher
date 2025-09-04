6e9ae7cbccbf8e6336ebb738fb5c95ae1303f747
Oghenesuvwe-dev <oghenesuvweomashone@hotmail.com>
2025-09-04 03:57:16 +0100

    fix: Exclude search page from build to resolve SSR issues

--- gatsby-node.js
+++ gatsby-node.js
@@ -9,7 +9,7 @@ exports.onCreatePage = ({ page, actions }) => {
   const { deletePage } = actions;
   
   // Skip pages that have SSR issues with react-slick
-  if (page.path.includes('/account/') || page.path.includes('/shopV2/') || page.path === '/shop/') {
+  if (page.path.includes('/account/') || page.path.includes('/shopV2/') || page.path === '/shop/' || page.path === '/search/') {
     deletePage(page);
   }
 };
